fb072e461cbce957b0e55102edc69ef6d19b3f66
zafar-m3n <thezafar.m3n@gmail.com>
2024-10-30 19:37:01 +0530

    Iconify

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Icon from "@/components/ui/Icon";
 function App() {
   return (
     <>
-      <h1 className="text-2xl font-bold underline text-center">Hello World!</h1>
+      <h1 className="text-2xl font-bold underline text-center font-body">
+        Hello World!
+      </h1>
+      <Icon icon="bx:bx-home" className="text-4xl text-center" />
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,15 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ["Syne", "sans-serif"], 
+        body: ["Mulish", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
